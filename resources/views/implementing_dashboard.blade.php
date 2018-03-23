e26df1495b73fecd8649aa0b6c9df455dcd79379
manas <manasshukla997@gmail.com>
2018-03-23 16:08:29 +0530

    Dashboard template

---
+++ resources/views/implementing_dashboard.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+
+    <h1 class="text-center">PROJECTS</h1><br><br>
+
+    <div class="row">
+        <div class="col-md-2">
+            <p>Project 1</p>
+        </div>
+        <div class="col-md-2">
+            <p>Project 2</p>
+        </div>
+        <div class="col-md-2">
+            <p>Project 3</p>
+        </div>
+        <div class="col-md-2">
+            <p>Project 4</p>
+        </div>
+        <div class="col-md-2">
+            <p>Project 5</p>
+        </div>
+        <div class="col-md-2">
+            <p>Project 6</p>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
