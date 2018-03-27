25145f2a3b14f600d11de8b3245c439b4cef0547
Priyanka <priyanka.lubal33@gmail.com>
2018-03-27 21:47:06 +0530

    nodal phase3

--- app/Http/Controllers/Phase2Controller.php
+++ app/Http/Controllers/Phase2Controller.php
@@ -16,4 +16,9 @@ class Phase2Controller extends Controller
         Phase2::create($request->all());
         return redirect()->route('implementing_phase2');
     }
+
+    public function displayNodal(){
+        $phase2 = Phase2::find(1); //Write the id from the table 'id'
+        return view('nodal_phase2')->with('phase2',$phase2);
+    }
 }
