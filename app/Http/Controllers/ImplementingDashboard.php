577cb0400bb6babfa3d9754bbdfe0c32e10e8cc4
yora98 <yrsmahajan@gmail.com>
2018-03-30 18:32:46 +0530

    implementingdashboard

--- app/Http/Controllers/ImplementingDashboard.php
+++ app/Http/Controllers/ImplementingDashboard.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Phase1;
 use App\Project;
 use Illuminate\Support\Facades\Auth;
@@ -16,5 +17,11 @@ class ImplementingDashboard extends Controller
           $phase1=Phase1::create(['status'=>'0']);
           $project=Project::create(['phase1_id'=>$phase1->id,'implementing_agency_id'=>$id]);
           return redirect()->route('implementing_phase1',$phase1->id);
+    }
+    public function getProject(){
+        $id = Auth::id();
+        //$userproject=Project::where('implementing_agency_id',$id)->get();
+        $userproject=DB::table('projects')->join('phase1', 'projects.phase1_id', '=', 'phase1.id')->where('implementing_agency_id',$id)->get();
+        return view('new_implement_dash')->with('userproject',$userproject);
     }
 }
