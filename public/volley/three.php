793079ad1e7598eef74e3ecad4ba7f48b89a3fb2
manas <manasshukla997@gmail.com>
2018-03-31 14:27:04 +0530

    login and register UI Theme

---
+++ public/volley/three.php
@@ -0,0 +1,43 @@
+
+<?php
+	$db_name = "dpr";
+	$mysql_user = "root";
+	$mysql_pass = "";
+	$server_name = "localhost";
+	
+	$conn = mysqli_connect($server_name, $mysql_user, $mysql_pass, $db_name);
+	
+	$response = array();
+	
+	if(!$conn){
+		//echo ("Connection Error");
+		$response['conn'] = "Connection Issue";
+	}
+	
+	else{
+		//echo ("Connection Success!");
+		$response['conn'] = "Connection Success";
+	}
+	
+	$state_name = $_POST['state_name'];
+	$district_name = $_POST['district_name'];
+	$city_name = $_POST['city_name'];
+	$project_name = $_POST['project_name'];
+	$project_code = $_POST['project_code'];
+	$nodal_agency = $_POST['nodal_agency'];
+	$implementing_agency = $_POST['implementing_agency'];
+	$implementing_agency_name = $_POST['implementing_agency_name'];
+		
+		$response['recMail'] = $emailVar;
+		
+		$query = "INSERT INTO phase2 (id, state_name, district_name, city_name, project_name, project_code, nodal_agency, implementing_agency, implementing_agency_name, approval_date) VALUES (NULL, '".$state_name."', '".$district_name."', '".$city_name."', '".$project_name."', '".$project_code."', '".$nodal_agency."', '".$implementing_agency."', '".$implementing_agency_name."')";
+		
+		$result = mysqli_query($conn,$query);
+		
+		if(!$result){
+		$response['push'] = "Not Success";
+		}else{
+		$response['push'] = "Success";
+		}
+
+?>
