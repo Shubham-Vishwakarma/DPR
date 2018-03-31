1c72d80411fa840077f82cb25ad74c29109a22bf
manas <manasshukla997@gmail.com>
2018-03-31 15:23:07 +0530

    BS update

--- resources/views/implementing_phase2.blade.php
+++ resources/views/implementing_phase2.blade.php
@@ -10,6 +10,11 @@
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+        <link rel="icon" href="/img/header.png">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
         <title>Phase 2</title>
     </head>
 
--- resources/views/nodal_phase3.blade.php
+++ resources/views/nodal_phase3.blade.php
@@ -8,6 +8,9 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="icon" href="/img/header.png">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <style>
     .backcolor {
       background-color: yellow;
